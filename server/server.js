bfe79a52492d5fc2aa85548608c6b6c91fc7ce04
Grejo Joby <grejo00@gmail.com>
2021-05-12 19:36:10 +0530

    origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const defaultValue = ""
 const io = require('socket.io')(PORT, {
     cors: {
         // origin: 'https://lit-chamber-84718.herokuapp.com',
-        origin: 'https://609bdd26c5828300077757a4--optimistic-shockley-bfa68b.netlify.app/',
+        origin: '*',
         methods: ["GET", "POST"],
     },
 })
