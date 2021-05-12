ae4bba99dccc4ec1779f22de5a2ed9952eb13ff1
Grejo Joby <grejo00@gmail.com>
2021-05-12 19:56:36 +0530

    route change

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,11 @@ import { v4 as uuidV4 } from 'uuid'
 function App() {
   return <Router>
     <Switch>
-      <Route path="/" exact >
-        <Redirect to={`/documents/${uuidV4()}`} />
-      </Route>
       <Route path="/documents/:id">
         <TextEditor />
+      </Route>
+      <Route path="/" >
+        <Redirect to={`/documents/${uuidV4()}`} />
       </Route>
     </Switch>
   </Router>;
