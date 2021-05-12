1cd5012ced5395850b507d7f347a45e85b64ae18
Grejo Joby <grejo00@gmail.com>
2021-05-12 20:09:20 +0530

    component

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,12 @@ import { v4 as uuidV4 } from 'uuid'
 function App() {
   return <Router>
     <Switch>
-      <Route path="/documents/:id">
-        <TextEditor />
-      </Route>
-      <Route path="/" >
+      <Route exact path="/" >
         <Redirect to={`/documents/${uuidV4()}`} />
       </Route>
+      <Route exact path="/documents/:id" component={TextEditor} />
+        {/* <TextEditor /> */}
+      {/* </Route> */}
     </Switch>
   </Router>;
 }
